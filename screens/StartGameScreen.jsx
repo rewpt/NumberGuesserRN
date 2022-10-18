38d085c556e454896e668f4763fa77f6587c83cb
rewpt <zpermack@gmail.com>
2022-10-18 17:41:12 -0400

    change react dom to 18.0.0

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,7 +1,40 @@
-import { View, TextInput } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
-  return <div>StartGameScreen</div>;
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2} />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: '#72063c',
+    elevation: 4, //android only
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: 45,
+  },
+});
